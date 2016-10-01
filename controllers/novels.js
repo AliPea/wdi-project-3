de831a0fb8d58ca67aad2bceed507ec444cfbc80
Sdraje <alfmarmil@gmail.com>
2016-10-01 16:05:18 +0100

    Added author in seeds and backend controller

--- controllers/novels.js
+++ controllers/novels.js
@@ -23,7 +23,9 @@ function novelsCreate( req, res ) {
 }
 
 function novelsShow( req, res ) {
-  Novel.findById(req.params.id, (err, novel) => {
+  Novel.findById(req.params.id)
+  .populate('entries.author')
+  .exec((err, novel) => {
     if(err) return res.status(500).json({ message: "Something went wrong"});
     return res.status(200).json({ novel });
   });
