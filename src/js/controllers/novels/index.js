4c3c778afaaa1cf7c451427a634a73936cd10dca
Sdraje <alfmarmil@gmail.com>
2016-10-01 12:20:46 +0100

    Fixed display of novels index and show

--- src/js/controllers/novels/index.js
+++ src/js/controllers/novels/index.js
@@ -1,5 +1,4 @@
 angular
-<<<<<<< HEAD
 .module("noveList")
 .controller("NovelIndexCtrl", NovelIndexCtrl);
 
@@ -11,17 +10,6 @@ function NovelIndexCtrl(Novel, $http){
   .query()
   .$promise
   .then(data => {
-    console.log('indexjs data', data)
     vm.novels = data.novels;
-=======
-  .module("noveList")
-  .controller("NovelIndexCtrl", NovelIndexCtrl);
-
-NovelIndexCtrl.$inject = ["Novel", "$stateParams", "$state"];
-function NovelIndexCtrl(Novel){
-  const vm   = this;
-  Novel.query(data => {
-    vm.novel = data.novels;
->>>>>>> ec909fc599b7dc9da83585bf900a41b40f9b9831
   });
 }
