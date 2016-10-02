426117d31db40e74175a80646b2b6ae50f0639c4
Sdraje <alfmarmil@gmail.com>
2016-10-02 15:40:53 +0100

    Added new novel

---
+++ src/js/factories/randomImage.js
@@ -0,0 +1,10 @@
+angular
+.module("noveList")
+.factory("RandomImage", RandomImage);
+
+RandomImage.$inject = ["$resource"];
+function RandomImage($resource) {
+  return $resource('https://api.unsplash.com/photos/random?client_id=19e34e75471ee865d7276aade14a9c897fe6dc4ba68d8d2058c627ed81ac0bb0', null, {
+    'query':  { method: "GET", isArray: false }
+  });
+}
