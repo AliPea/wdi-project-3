2f394c9a11512939db801290bb5965a3cbf091bc
samyounger <sam_younger@hotmail.com>
2016-10-02 14:46:36 +0100

    comments functionality added

--- controllers/novels.js
+++ controllers/novels.js
@@ -1,10 +1,11 @@
 module.exports = {
-  index:    novelsIndex,
-  create:   novelsCreate,
-  show:     novelsShow,
-  update:   novelsUpdate,
-  delete:   novelsDelete,
-  addEntry: novelsAddEntry
+  index:      novelsIndex,
+  create:     novelsCreate,
+  show:       novelsShow,
+  update:     novelsUpdate,
+  delete:     novelsDelete,
+  addEntry:   novelsAddEntry,
+  addComment: novelsAddComment
 };
 
 const Novel = require('../models/novel');
@@ -55,6 +56,22 @@ function novelsAddEntry(req, res) {
 
     novel.entries.addToSet({
       body: req.body.entry,
+      author: req.user._id
+    });
+
+    novel.save((err, novel) => {
+      if (err) return res.status(500).json({ message: "Something went wrong"});
+      return res.status(201).json({ novel });
+    });
+  });
+}
+
+function novelsAddComment(req, res) {
+  Novel.findById(req.params.id, (err, novel) => {
+    if (err) return res.status(500).json({ message: "Something went wrong"});
+    if (!novel) return res.status(404).json({ message: "Novel not found"});
+    novel.comments.addToSet({
+      body: req.body.comment,
       author: req.user._id
     });
 
--- src/js/controllers/novels/show.js
+++ src/js/controllers/novels/show.js
@@ -10,16 +10,33 @@ function NovelShowCtrl(Novel, $stateParams, $state) {
   Novel.get($stateParams, data => {
     vm.novel = data.novel;
   });
+  // 
+  // vm.filter('reverse', function() {
+  //   return function(items) {
+  //     return items.slice().reverse();
+  //   };
+  // });
 
   // Get formData & update the novel
-  vm.submit = () => {
+  vm.submitEntry = () => {
     console.log(vm.novel.entries.body);
     Novel
-      .addEntry($stateParams, { entry: vm.novel.entries.body })
-      .$promise
-      .then(data => {
-        $state.go("novelShow", $stateParams);
-      });
+    .addEntry($stateParams, { entry: vm.novel.entries.body })
+    .$promise
+    .then(data => {
+      vm.novel.entries.push(data.novel.comments);
+      vm.novel.entries.body = null;
+    });
+  };
+
+  vm.submitComment = () => {
+    Novel
+    .addComment($stateParams, { comment: vm.novel.comments.body })
+    .$promise
+    .then(data => {
+      vm.novel.comments.push(data.novel.comments);
+      vm.novel.comments.body = null;
+    });
   };
 
   vm.novelDelete = () => {
