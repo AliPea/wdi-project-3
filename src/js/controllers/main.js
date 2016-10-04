a3bd05f10f5fb0360a5ed835f3e928fe55cbd9d0
samyounger <sam_younger@hotmail.com>
2016-10-04 16:02:12 +0100

    working on showCtrl & novelShow.html, with styling mixedIn

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -15,7 +15,7 @@ function mainCtrl($rootScope, CurrentUserService, $state) {
 
   $rootScope.$on("loggedIn", () => {
     vm.user = CurrentUserService.getUser();
-    $state.go("home");
+    $state.go("novelIndex");
   });
 
   $rootScope.$on("loggedOut", () => {
